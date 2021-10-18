a795fc9829f7f776124d78cee9ee15317334d1e1
Antonio Ochoa <antonhub00@gmail.com>
2021-10-18 09:13:48 -0500

    Project setup

---
+++ number_base_converter.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace number_base_converter.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ number_base_converter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace number_base_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
